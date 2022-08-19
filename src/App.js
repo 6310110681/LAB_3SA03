ed062f323668de3eae5eb525085eb6d8b007987e
Onint <ekcept2467@gmail.com>
2022-08-20 02:39:54 +0700

    character two

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import CharacterCard from './CharacterCard';
 
 function App() {
   return (
-    <div className="App">
-      CharacterCard
+    <div className='App'>
+      <Character/>
+      <Character/>
     </div>
   );
 }
