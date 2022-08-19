e772fefc466c74d72206ebfa39acd2c730496ccd
Onint <ekcept2467@gmail.com>
2022-08-20 03:35:10 +0700

    Components & States three (Handler as Props)

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -2,10 +2,12 @@ import React, { useState } from 'react';
 
 export default function CharacterCard(props) { 
     const [active, setActive] = useState(false); 
-    
-    const activate = () => {  
-        setActive(true)
-        props.activationHandler(props.value)    
+
+    const activate = () => { 
+        if(!active){ 
+            setActive(true)
+            props.activationHandler(props.value)
+        }    
     } 
 
     const className = `card ${active ? 'activeCard': ''}`     
