8a936f7690b93b922dcec0fbe076d9fdc9517010
Onint <ekcept2467@gmail.com>
2022-08-20 02:32:47 +0700

    character

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import CharacterCard from './CharacterCard';
 
 function App() {
   return (
     <div className="App">
-      hello world
+      CharacterCard
     </div>
   );
 }
---
+++ src/CharacterCard.js
@@ -0,0 +1,6 @@
+import React from 'react'; 
+export default function CharacterCard() {
+    return (         
+         <div>characterC</div>     
+         )     
+    }  
