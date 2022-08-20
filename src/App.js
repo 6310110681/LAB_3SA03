68315b52f9ad3049330401b13f95697d5ac9bcd8
Onint <ekcept2467@gmail.com>
2022-08-20 11:18:07 +0700

    Assignment : Add Header

--- src/App.js
+++ src/App.js
@@ -6,6 +6,14 @@ const word = "Country";
 function App() {
   return (
     <div>
+      <div class="value">
+        <header>
+          <h1>WORD GAME</h1>
+        </header>
+        <h2>Country In Asia</h2>
+        <p>Please, Spell the word correctly.</p>
+    </div>
+
       <WordCard value="Thailand"/>
       <WordCard value="Korea"/>
       <WordCard value="Japan"/>
