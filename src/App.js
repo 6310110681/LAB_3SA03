1ca4a0c4b6341d7fbec995f2ddf70f7f493576e6
Onint <ekcept2467@gmail.com>
2022-08-20 12:55:17 +0700

    Assignment : Add Hint

--- src/App.js
+++ src/App.js
@@ -15,16 +15,28 @@ function App() {
     </div>
 
       <WordCard value="Thailand"/>
+      <p>Hint : The country With Longest Capital Names In The World.</p>
+      <img src="thailand.jpg"></img>
       <WordCard value="Korea"/>
+      <p>Hint : The country with HANBOK as their national costume.</p>
+      <img src="korea.jpg"></img>
       <WordCard value="Japan"/>
+      <p>Hint : The country that have FUJI Mount.</p>
+      <img src="japan.jpg"></img>
       <WordCard value="India"/>
+      <p>Hint : The country has a tourist attraction that symbolizes love known as the Taj Mahal.</p>
+      <img src="india.jpg"></img>
       <WordCard value="Hongkong"/>
+      <p>Hint : The country where the Transformers movie was filmed was at the Yick Fat Building.</p>
+      <img src="hongkong.jpg"></img>
 
     <div>
+      <br></br><br></br><br></br>
       <p>ONINT CHOOZENG STUDEN ID : 6310110681</p>
     </div>
 
     </div>
+    
   );
 }
 
